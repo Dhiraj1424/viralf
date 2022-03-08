debde033db18377ead617294b3aaaadb3155bd57
Dhiraj Subedi <dhirajsubedi924@gmail.com>
2022-03-08 15:29:29 +0545

    gestrue/inkwell splashColor

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:viralf/home.dart';
 import 'package:viralf/utlis/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name="";
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,8 +26,8 @@ class LoginPage extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              const Text(
-                'Welcome',
+              Text(
+                'Welcome $name',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
               const SizedBox(
@@ -31,38 +38,68 @@ class LoginPage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextFormField(
-                      decoration: const InputDecoration(
+                      decoration:  InputDecoration(
                           hintText: "enter user name", labelText: "User name"),
+                          onChanged: (value){
+                            name=value;
+                            setState(() {
+                              
+                            });
+                          }
                     ),
                     TextFormField(
                       obscureText: true,
-                      decoration: const InputDecoration(
+                      decoration:  InputDecoration(
                           hintText: "enter password", labelText: "Password"),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 30,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
-                        // Navigator.pushReplacementNamed(context, "/");
-                        Navigator.pushNamed(context, MyRoute.homeRoute);
-                      },
-                      child: Text('Login'),
-                      // style: ButtonStyle(
-                      //     backgroundColor: MaterialStateProperty.all(Colors.cyan),
-                      //     shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      //         borderRadius: BorderRadius.circular(10),
-                      //         side: BorderSide(color: Colors.red, width: 3)))),
-        
-                      style:ElevatedButton.styleFrom(
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoute.homeRoute);
+                       
+                    //   },
+                      
+                    //   child: Text('Login'),
+                      
+                    //   style:ElevatedButton.styleFrom(
                         
-                        side: BorderSide(color: Colors.red, width: 2),
-                        shape: RoundedRectangleBorder(
+                    //     side: BorderSide(color: Colors.red, width: 2),
+                    //     shape: RoundedRectangleBorder(
                           
-                          borderRadius: BorderRadius.circular(10)
-                        )
-                      )
+                    //       borderRadius: BorderRadius.circular(10)
+                    //     )
+                    //   )
+                    // )
+
+                    Material(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.purple,
+                      child: InkWell(
+                        splashColor: Colors.white,
+                        onTap: (){
+                          Navigator.pushNamed(context, MyRoute.homeRoute);
+                        },
+                        child: Container(
+                      
+                          width: 100,
+                          height: 50,
+                          // decoration: const BoxDecoration(
+                            
+                          //   color: Colors.blue,
+                          // borderRadius: BorderRadius.only(
+                          //   topLeft: Radius.circular(10),
+                          //   bottomLeft: Radius.circular(10)
+                          // ),
+                          
+                         
+                          // ),
+                          
+                          alignment: Alignment.center,
+                          child: const Text('Login'),
+                        ),
+                      ),
                     )
                   ],
                 ),
